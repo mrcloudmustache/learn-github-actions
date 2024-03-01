641bdecec1dcbfed228311cdffcc573d1a39d1be
thom8153 <thom8153@umn.edu>
2024-03-01 15:27:21 -0600

    added s3 remote backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region     = "us-east-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "mcmgithubactionstfstate"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "mcmgithubactions-state-locking"
+    encrypt = true
+    
+  }
+}
